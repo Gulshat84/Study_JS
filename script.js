3ef2bcae1aa8b5113aa1c905c258109675a07cc6
Gulshat Nugumanova <gulshatik.nugumanova84@gmail.com>
2021-01-28 20:15:47 +0300

    Созданы переменные

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 alert("Hello, friends!");
-[1,2,3].push();
+
 console.log("Hello!");
 
 let agePerson = 20;
-console.log('agePerson = 20: ', agePerson = 20);
+console.log(agePerson);
+
+let money;
+
+let income;
+
+let addExpenses;
+
+let deposit;
+
+let mission;
+
+let period;
