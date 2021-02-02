f521bc8e5b395a281172a1581cb13045d7ba7f5d
Gulshat Nugumanova <gulshatik.nugumanova84@gmail.com>
2021-02-02 17:45:09 +0300

    Внесены изменения в файл JS

--- script.js
+++ script.js
@@ -3,32 +3,76 @@ console.log("Hello!");
 let agePerson = 20;
 console.log(agePerson);
 
-// Доход за месяц
-let money = 30000;
-console.log(typeof money);
 
 // Дополнительный доход
 let income = 'Фриланс';
 console.log(typeof income);
 
-// Дополнительные расходы
-let addExpenses = 'Интернет Такси Коммуналка';
+
+// Доход за месяц
+let money = +prompt('Ваш месячный доход?');
+console.log(typeof money);
+console.log(money);
+
+
+
+// Возможные расходы
+let addExpenses = 'Интернет, Такси, Коммуналка, Продукты';
 console.log(addExpenses.length);
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(' '));
 
+let expenses1 = prompt('Введите обязательную статью расходов');
+console.log(expenses1);
+
+let amount1 = +prompt('Во сколько это обойдётся');
+console.log(amount1);
+
+let expenses2 = prompt('Введите обязательную статью расходов');
+console.log(expenses2);
+
+let amount2 = +prompt('Во сколько это обойдётся');
+console.log(amount2);
+
+let expenses = amount1 + amount2;
+console.log(expenses);
+console.log(typeof expenses);
+
+
+
 // Булевое значение
-let deposit = true;
+let deposit = confirm('Есть ли у вас депозит в банке?');
 console.log(typeof deposit);
+console.log(deposit);
 
 // Желаемая сумма накопления
-let mission = 5000000;
+let mission = 200000;
 console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
+console.log(typeof mission);
+
+// Бюджет на месяц
+let budgetMonth = money-expenses;
+
+console.log(typeof budgetMonth);
+
 
 // Период от 1 до 12 месяцев
-let period = 6;
-console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+let period = mission/budgetMonth;
+console.log('Период равен' + ' ' + Math.ceil(period) + ' ' + 'месяцев');
+console.log(Math.ceil(period));
 
 // Дневной бюджет
-let budgetDay = money / 30;
-console.log(budgetDay);
+let budgetDay = budgetMonth/30;
+console.log(Math.floor(budgetDay));
+
+// Уровень дохода
+if (budgetDay>1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay>600) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay<600) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay<0) {
+  console.log('Что-то пошло не так');
+};
+
